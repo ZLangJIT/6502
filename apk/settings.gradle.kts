c9cf7e0c6bc6eeaecb5f4a5e292b9b9d2ebdbf4b
mgood7123 <smallville7123@gmail.com>
2024-11-26 02:49:36 +0000

    build

--- apk/settings.gradle.kts
+++ apk/settings.gradle.kts
@@ -1,12 +1,17 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         maven { url = uri("https://jitpack.io") }
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
