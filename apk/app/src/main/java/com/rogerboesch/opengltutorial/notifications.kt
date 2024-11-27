a08536f244a2bc96c15c82eeeae27b671586c615
mgood7123 <smallville7123@gmail.com>
2024-11-27 00:14:03 +0000

    build

--- apk/app/src/main/java/com/rogerboesch/opengltutorial/notifications.kt
+++ apk/app/src/main/java/com/rogerboesch/opengltutorial/notifications.kt
@@ -6,6 +6,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.media.RemoteControlClient
