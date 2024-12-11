8c879c55b09462c187f33553d318b83f0eea4d59
ZLangJIT <smallville7123@gmail.com>
2024-12-11 11:30:34 +0000

    build

--- build.sh
+++ build.sh
@@ -11,22 +11,45 @@ if $(am --help 2>&1 | grep -q -i "to-intent-uri") ; then
     apt update
     apt upgrade
     #apt install cmake clang mesa-dev
-    apt install wget cmake
-    if [[ ! -e ndk ]] ; then
-      if [[ ! -e android-ndk-r27b-aarch64.zip ]] ; then
+    apt install wget
+    if [[ ! -e android_sdk ]] ; then
+      mkdir android_sdk
+      cd android_sdk
+      NDK_URL=https://github.com/lzhiyong/termux-ndk/releases/download/android-ndk/android-ndk-r27b-aarch64.zip
+      SDK_URL=https://github.com/lzhiyong/termux-ndk/releases/download/android-sdk/android-sdk-aarch64.zip
+      JAVA_URL=https://github.com/lzhiyong/termux-ndk/releases/download/android-openjdk/openjdk-21.0.1-aarch64.zip
+      if [[ ! -e $(basename $NDK_URL) ]] ; then
         echo "downloading ndk ..."
-        wget https://github.com/lzhiyong/termux-ndk/releases/download/android-ndk/android-ndk-r27b-aarch64.zip
+        wget $NDK_URL
       fi
-      mkdir ndk
-      cd ndk
       echo "extracting ndk ..."
-      unzip ../android-ndk-r27b-aarch64.zip
-      if [[ -e ../android-ndk-r27b-aarch64.zip ]] ; then
-        rm ../android-ndk-r27b-aarch64.zip
+      unzip $(basename $NDK_URL)
+      if [[ -e $(basename $NDK_URL) ]] ; then
+        rm $(basename $NDK_URL)
+      fi
+      if [[ ! -e $(basename $SDK_URL) ]] ; then
+        echo "downloading sdk ..."
+        wget $SDK_URL
+      fi
+      echo "extracting sdk ..."
+      unzip $(basename $SDK_URL)
+      if [[ -e $(basename $SDK_URL) ]] ; then
+        rm $(basename $SDK_URL)
+      fi
+      if [[ ! -e $(basename $JAVA_URL) ]] ; then
+        echo "downloading java ..."
+        wget $JAVA_URL
+      fi
+      echo "extracting java ..."
+      unzip $(basename $JAVA_URL)
+      if [[ -e $(basename $JAVA_URL) ]] ; then
+        rm $(basename $JAVA_URL)
       fi
       cd ..
     fi
-    export ANDROID_NDK_HOME=$(pwd)/ndk/android-ndk-r27b
+    export JAVA_HOME=$(pwd)/android_sdk/java
+    export ANDROID_SDK_HOME=$(pwd)/android_sdk/android-sdk
+    export ANDROID_NDK_HOME=$(pwd)/android_sdk/android-ndk-r27b
     export PYTHONHOME=$(ANDROID_NDK_HOME)/toolchains/llvm/prebuilt/linux-aarch64/python3
     export LD_LIBRARY_PATH=$(ANDROID_NDK_HOME)/toolchains/llvm/prebuilt/linux-aarch64/python3/lib
     cmake -DCMAKE_BUILD_TYPE=Release -DTERMUX=1 -S . -B build
