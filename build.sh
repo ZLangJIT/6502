1362014692953b2df2f527b6167848903e37fe5c
ZLangJIT <smallville7123@gmail.com>
2024-12-11 10:39:00 +0000

    build

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ if $(am --help 2>&1 | grep -q -i "to-intent-uri") ; then
       mkdir ndk
       cd ndk
       echo "extracting ndk ..."
-      unzip https://github.com/lzhiyong/termux-ndk/releases/download/android-ndk/android-ndk-r27b-aarch64.zip
+      unzip android-ndk-r27b-aarch64.zip
       cd ..
     fi
     export ANDROID_NDK_HOME=$(pwd)/ndk
