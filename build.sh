976f49533a205e08e186cc954df071b95a6cc2f1
ZLangJIT <smallville7123@gmail.com>
2024-12-11 10:34:38 +0000

    build

--- build.sh
+++ build.sh
@@ -10,7 +10,18 @@ if $(am --help 2>&1 | grep -q -i "to-intent-uri") ; then
     echo "Termux shell detected"
     apt update
     apt upgrade
-    apt install cmake clang mesa-dev
+    #apt install cmake clang mesa-dev
+    apt install wget
+    if [[ ! -e ndk ]] ; then
+      echo "downloading ndk ..."
+      wget https://github.com/lzhiyong/termux-ndk/releases/download/android-ndk/android-ndk-r27b-aarch64.zip
+      mkdir ndk
+      cd ndk
+      echo "extracting ndk ..."
+      unzip https://github.com/lzhiyong/termux-ndk/releases/download/android-ndk/android-ndk-r27b-aarch64.zip
+      cd ..
+    fi
+    export ANDROID_NDK_HOME=$(pwd)/ndk
     cmake -DCMAKE_BUILD_TYPE=Release -DTERMUX=1 -S . -B build
   else
     echo "Non-Termux shell detected, build might fail"
